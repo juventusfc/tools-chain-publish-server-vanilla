5442c4398fff11fa23a902faa50b0161eceb4943
FrankHu <frank.hu@perficient.com>
2020-08-19 14:24:29 +0800

    typo

---
+++ README.md
@@ -0,0 +1,3 @@
+# publish server vanilla
+
+基于 http 搭建的 publish server，支持 http 流式传输。
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const http = require("http");
 const fs = require("fs");
+const unzip = require("unzipper");
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-  let matched = req.url.match(/filename=([^&]+)/);
-  let fileName = matched && matched[1];
-  if (!fileName) return;
-  let writeStream = fs.createWriteStream("../server/public/" + fileName);
+  let writeStream = unzip.Extract({ path: "../server/public/package" });
   req.pipe(writeStream);
   req.on("end", () => {
     res.writeHead(200, { "Content-Type": "text/plain" });
